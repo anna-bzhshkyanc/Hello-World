bb52019c0342c170bbaa12e1f5d0e60d2505db2a
Anna Bzhshkyanc <annabzhshkyan@192.168.27.4>
2025-05-15 13:15:14 +0400

    initial project

---
+++ Hello-World/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hello-World
+//
+//  Created by Anna Bzhshkyan on 15.05.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hello-World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello-World
+//
+//  Created by Anna Bzhshkyan on 15.05.25.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
